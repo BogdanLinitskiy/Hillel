1e73977af13639485a8d680f80e2377f34879941
Bogdan Linitskiy <titan4ek@gmail.com>
2017-12-19 17:27:59 +0200

    final commit

--- hw_12/index.php
+++ hw_12/index.php
@@ -2,7 +2,7 @@
 echo '<meta charset="utf-8">';
 $search_words = ['php','html','интернет','Web'];
 
-$search_string = ['Интернет - большая сеть компьютеров, которые могут взаимодействовать друг с другом.',
+$search_strings = ['Интернет - большая сеть компьютеров, которые могут взаимодействовать друг с другом.',
     'PHP - это распространенный язык программирования с открытым исходным кодом.',
     'PHP сконструирован специально для ведения Web-разработок и его код может внедряться неспосредственно в HTML'
 ];
@@ -12,17 +12,17 @@ function regularCheck($words,$string)
     $map = [];
     for ($i = 0; $i < count($string); $i++) {
         foreach ($words as $word) {
-                if (preg_match("/$word/iu", $string[$i])) {
-                    $map[] = $word;
-                }
+            if (preg_match("/$word/iu", $string)) {
+                $map[] = $word;
             }
         }
-
+    }
     return $map;
 }
-$arr[] = regularCheck($search_words,$search_string);
-echo '<pre>';
-print_r($arr);
-
-print_r( regularCheck($search_words,$search_string));
-print_r($search_string);
+$arrCounter=1;
+for($i=0;$i<count($search_strings);$i++) {
+    $result=regularCheck($search_words, $search_strings[$i]);
+    echo "В предложении №$arrCounter есть слова: ";
+    echo implode(", ",$result).".<br>";
+    $arrCounter++;
+}
